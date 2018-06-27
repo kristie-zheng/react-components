df7acb19a315b5784a89015ecb98d82e1aba848b
Kristie Desiree <kly5@mail.sfsu.edu>
2018-06-27 11:14:08 -0700

    Set up the first component (grocery list) and rendered it

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+/*jshint esversion: 6*/
 // TODO
+
+var GroceryList = (item1, item2) => (
+  <ul>
+  <li>item1</li>
+  <li>item2</li>
+  </ul>
+);
+
+  ReactDOM.render(<GroceryList />, document.querySelector("#app"));
